eb951b284e18fc198d0e4d94e01ccb038850d83f
ignaciozanotti <ignacio.zanotti@gmail.com>
2020-05-07 13:20:07 -0300

    added hooks alternative

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import store from './redux/store'
 
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <CakeContainer />
+        <HooksCakeContainer />
       </div>
     </Provider>
 
---
+++ src/components/HooksCakeContainer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import {buyCake, stockCake} from '../redux'
+
+function HooksCakeContainer() {
+
+    const numOfCakes = useSelector(state => state.numOfCakes)
+    const dispatch = useDispatch()
+     
+    return (
+        <div>
+            <h2>Hooks Based Num of Cakes { numOfCakes }</h2>
+            <button onClick={() => dispatch(buyCake())}> Buy Cake </button>
+            <button onClick={() => dispatch(stockCake())}> Stock Cake </button>
+        </div>
+    )
+}
+
+export default HooksCakeContainer
